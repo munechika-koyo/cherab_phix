bfa8955d6d5b389b88666da470ebd48c32738d04
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2021-12-01 19:40:30 +0900

    [remove] delete original calibration codes.
    Because of recommending using the calcam package, deleted the originals

--- cherab/phix/observer/__init__.py
+++ cherab/phix/observer/__init__.py
@@ -1,5 +1,4 @@
 from .thin_lens_ccd import ThinLensCCDArray
 from .fast_camera.camera import import_phix_camera
-from .camera_calibration import Calibration
 
-__all__ = ["ThinLensCCDArray", "import_phix_camera", "Calibration"]
+__all__ = ["ThinLensCCDArray", "import_phix_camera"]
