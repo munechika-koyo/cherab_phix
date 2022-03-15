ae472ec060f8eb95846dfc6288117e147bdb9f7c
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2022-03-15 12:05:05 +0900

    [fix] uncorrect roles in docstring

--- cherab/phix/observer/thin_lens_ccd.pyx
+++ cherab/phix/observer/thin_lens_ccd.pyx
@@ -70,12 +70,12 @@ cdef class ThinLensCCDArray(Observer2D):
         Number of samples to generate on the thin Lens (default=100).
     per_pixel_samples : int
         Number of samples to generate per pixel (default=10).
-        which is different from `pixel_samples` in :py:class:`~raysect.optical.observer.base.Observer2D` object.
+        which is different from `pixel_samples` in :py:class:`~raysect.optical.observer.base.observer.Observer2D` object.
         When you use ThinLensCCDArray, `pixel_samples` = `per_pixel_samples` x `lens_samples` (default=100 x 10).
     pipelines : list
         The list of pipelines that will process the spectrum measured
         at each pixel by the camera (default= :py:class:`~raysect.optical.observer.pipeline.rgb.RGBPipeline2D`).
-    **kwargs : :py:class:`~raysect.optical.observer.base.Observer2D` and _ObserverBase. properties, optional
+    **kwargs : :py:class:`~raysect.optical.observer.base.observer.Observer2D` and _ObserverBase. properties, optional
         *kwargs* are used to specify properties like a parent, transform, pipelines, etc.
     """
 
