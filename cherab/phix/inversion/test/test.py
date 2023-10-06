beac2f476925fe7d8286f2f8460bd88892e5d988
Koyo Munechika <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-10-06 17:59:22 +0900

    🎨 update inversion test script

--- cherab/phix/inversion/test/test.py
+++ cherab/phix/inversion/test/test.py
@@ -36,15 +36,42 @@ if __name__ == "__main__":
     b_noise = rng.normal(0, 1.0e-4, b_0.size)
     b = b_0 + b_noise
 
-    # === inversion process
-    lcurve = Lcurve(sigma, u, vh, b)
-    lcurve.lambdas = 10 ** np.linspace(-20, 2, 100)
-    lcurve.optimize(4)
-    lcurve.plot_L_curve()
+    # inversion process
+    bounds = (-20.0, 2.0)
 
-    gcv = GCV(sigma, u, vh, b)
-    gcv.lambdas = 10 ** np.linspace(-20, 2, 100)
-    gcv.optimize()
-    gcv.plot_gcv()
+    print("L-curve method")
+    lcurve = Lcurve(sigma, u, vh.T, data=b)
+
+    # solve inverse problem using `optimize` method
+    sol1 = lcurve.optimize(itemax=4, bounds=bounds)
+    lambda1 = lcurve.lambda_opt
+    print(f"`optimize` method result :{lcurve.lambda_opt = :.3e}")
+
+    # solve inverse problem using `solve` method
+    sol2, status = lcurve.solve(bounds=bounds, niter=10, disp=True)
+    print(status)
+    print(f"`solve` method result :{lcurve.lambda_opt = :.3e}")
+
+    # plot L-curve
+    _, ax = lcurve.plot_L_curve(bounds=bounds, n_beta=500)
+    ax.scatter(lcurve.residual_norm(lambda1), lcurve.regularization_norm(lambda1), c="g", marker="x", label=f"$\\lambda_1$ = {lambda1:.2e}")
+    ax.legend()
+
+    # plot curvature
+    _, ax = lcurve.plot_curvature(bounds=(-9, -7), n_beta=500)
+    ax.scatter(lambda1, lcurve.curvature(lambda1), c="g", marker="x", label=f"$\\lambda_1$ = {lambda1:.2e}")
+    print("----------------------------------------")
+
+    print("GCV method")
+    gcv = GCV(sigma, u, vh.T, data=b)
+
+    # solve inverse problem using `optimize` method
+    sol3 = gcv.optimize(itemax=4, bounds=bounds)
+
+    # solve inverse problem using `solve` method
+    sol4 = gcv.solve(bounds=bounds, disp=True)
+
+    # plot GCV
+    gcv.plot_gcv(bounds=bounds, n_beta=500)
 
     plt.show()
