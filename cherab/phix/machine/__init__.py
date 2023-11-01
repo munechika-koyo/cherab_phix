434079f592f60cfb0be2faff2988d2402c6d24a3
munechika-koyo <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-11-01 18:54:31 +0900

    ✨ new plot tool for wall outline

--- cherab/phix/machine/__init__.py
+++ cherab/phix/machine/__init__.py
@@ -1,5 +1,5 @@
 """Subpackage for Machine-related modules."""
 from .pfc_mesh import import_phix_mesh, show_PFCs_3D
-from .wall_outline import plot_phix_wall_outline
+from .wall_outline import plot_wall_outline
 
-__all__ = ["import_phix_mesh", "show_PFCs_3D", "plot_phix_wall_outline"]
+__all__ = ["import_phix_mesh", "show_PFCs_3D", "plot_wall_outline"]
