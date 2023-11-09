46d60669342d18ac318289522cd2c0e7d7be5393
munechika-koyo <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-11-09 14:19:15 +0900

    ✨ Add `compute_svd` and `Mfr` to inversion package

--- cherab/phix/inversion/__init__.py
+++ cherab/phix/inversion/__init__.py
@@ -1,6 +1,7 @@
 """Subpackage for Inversion Problem."""
 from .gcv import GCV
-from .inversion import _SVDBase
+from .inversion import _SVDBase, compute_svd
 from .lcurve import Lcurve
+from .mfr import Mfr
 
-__all__ = ["_SVDBase", "Lcurve", "GCV"]
+__all__ = ["_SVDBase", "compute_svd", "Lcurve", "GCV", "Mfr"]
