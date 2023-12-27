6327095b07fab9e1dccf00ed4333b41d116b4928
munechika-koyo <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-12-28 00:23:27 +0900

    🎨 change `import_phix_mesh` to `load_pfc_mesh`
    And not use STL files to show meshes in 3D space

--- cherab/phix/machine/__init__.py
+++ cherab/phix/machine/__init__.py
@@ -1,5 +1,5 @@
 """Subpackage for Machine-related modules."""
-from .pfc_mesh import import_phix_mesh, show_PFCs_3D
+from .pfc_mesh import load_pfc_mesh, show_PFCs_3D
 from .wall_outline import plot_wall_outline
 
-__all__ = ["import_phix_mesh", "show_PFCs_3D", "plot_wall_outline"]
+__all__ = ["load_pfc_mesh", "show_PFCs_3D", "plot_wall_outline"]
